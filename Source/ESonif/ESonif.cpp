9a9de67c83356f7b1080b898cfc5167c949531ca
Juan Del Rio <juandrh2.0@gmail.com>
2022-10-08 11:50:14 +0200

    Initial commit

---
+++ Source/ESonif/ESonif.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ESonif.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ESonif, "ESonif" );
---
+++ Source/ESonif/ESonifGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ESonifGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESONIF_API AESonifGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
